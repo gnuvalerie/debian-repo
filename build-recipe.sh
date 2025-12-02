d56c50d1c3f98288f2e4a8edaa7cf5a0366536c8
gnuvalerie <240524768+gnuvalerie@users.noreply.github.com>
2025-12-02 22:22:48 +0100

    Use absolute path for recipe file in build script

--- build-recipe.sh
+++ build-recipe.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 recipe=$1
+recipe_abs=$(realpath $recipe)
 
-name=$(grep '^name:' $recipe | cut -d: -f2 | xargs)
-repo=$(grep '^repo:' $recipe | cut -d: -f2- | xargs)
-deps=$(awk '/^deps:/,/^[^ ]/ {if ($0 ~ /^  -/) print $2}' $recipe | tr '\n' ' ')
+name=$(grep '^name:' $recipe_abs | cut -d: -f2 | xargs)
+repo=$(grep '^repo:' $recipe_abs | cut -d: -f2- | xargs)
+deps=$(awk '/^deps:/,/^[^ ]/ {if ($0 ~ /^  -/) print $2}' $recipe_abs | tr '\n' ' ')
 
 apt install -y $deps
 
 git clone --recursive $repo src
 cd src
 
-build=$(awk '/^build: \|/,/^[^ ]/ {if (NR > 1 && $0 !~ /^[^ ]/) print}' $recipe)
+build=$(awk '/^build: \|/,0 {if (NR > 1) print}' $recipe_abs | sed '1d')
 eval "$build"
 
 dpkg-deb --build pkg ../pool/main/${name}.deb
